2b8b74cc2092f66a8db7ad4de2f062908c972a7a
Alberto <aldo10012@gmail.com>
2021-02-11 14:25:10 -0800

    Implement stack navigation

---
+++ bot-shop/Order.swift
@@ -0,0 +1,14 @@
+//
+//  Order.swift
+//  bot-shop
+//
+//  Created by Alberto Dominguez on 2/11/21.
+//
+
+import Foundation
+import UIKit
+
+struct Order {
+    var title: String
+    var image: UIImage
+}
--- bot-shop/OrderList.swift
+++ bot-shop/OrderList.swift
@@ -1,5 +1,5 @@
 //
-//  PastOrderViewController.swift
+//  OrderList.swift
 //  bot-shop
 //
 //  Created by Alberto Dominguez on 2/11/21.
@@ -8,19 +8,17 @@
 import Foundation
 import UIKit
 
-class PastOrderViewController: UIViewController {
+class OrderList: UIViewController {
+
+    let orders = [Order(title: "July 2020", image: UIImage(named: "box")!),
+    Order(title: "June 2020", image: UIImage(named: "box")!),
+    Order(title: "May 2020", image: UIImage(named: "box")!),
+    Order(title: "December 2019", image: UIImage(named: "box")!),
+    Order(title: "November 2019", image: UIImage(named: "box")!),
+    Order(title: "October 2019", image: UIImage(named: "box")!),
+    Order(title: "September 2019", image: UIImage(named: "box")!)]
 
     let tableView =  UITableView()
-    
-    let orders = [
-        Order(title: "July 2020", image: UIImage(named: "box")!),
-        Order(title: "June 2020", image: UIImage(named: "box")!),
-        Order(title: "May 2020", image: UIImage(named: "box")!),
-        Order(title: "December 2019", image: UIImage(named: "box")!),
-        Order(title: "November 2019", image: UIImage(named: "box")!),
-        Order(title: "October 2019", image: UIImage(named: "box")!),
-        Order(title: "September 2019", image: UIImage(named: "box")!)
-    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,35 +38,24 @@ class PastOrderViewController: UIViewController {
         tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
         tableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
     }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 100.0
-    }
 
 }
 
-extension PastOrderViewController: UITableViewDataSource, UITableViewDelegate {
+extension OrderList: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return orders.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! PastOrderCell
-        cell.accessoryType = .disclosureIndicator
-        cell.selectionStyle = .none
-        cell.setBoxContents(box: orders[indexPath.row])
+            cell.accessoryType = .disclosureIndicator
+            cell.selectionStyle = .none
+            cell.setBoxContents(box: orders[indexPath.row])
         return cell
     }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print("selected!")
-        let nextVC: OrderList = OrderList()
-        self.navigationController?.pushViewController(nextVC, animated: true)
-    }
-    
-//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-//        return 100.0
-//    }
 
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100.0
+    }
 
 }
