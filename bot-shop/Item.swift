4bb030d37034be328301196c57f09d1e8ec0ab24
Alberto <aldo10012@gmail.com>
2021-02-11 13:32:53 -0800

    Users can see items for sale in collection view

---
+++ bot-shop/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  bot-shop
+//
+//  Created by Alberto Dominguez on 2/11/21.
+//
+
+import Foundation
+import UIKit
+
+struct Item {
+    var title: String
+    var image: UIImage
+}
